f844bc9a8fb41b27732881329681617f7707cb13
TiagoPerri <tiagocnsr@gmail.com>
2021-11-30 01:53:52 -0300

    Refatorando client.js

--- client.js
+++ client.js
@@ -1,40 +1,53 @@
 const axios = require('axios').default;
 
-axios.post('http://localhost:3000/books', {
-    ID: 3,
-    name: 'My Boook',
-    author: 'Me and only me'
-})
-    .then((res) => {
-        console.log(res.data);
+async function addNewBook() {
+    await axios.post('http://localhost:3000/books', {
+        ID: 3,
+        name: 'My Boook',
+        author: 'Me and only me'
     })
-    .catch(err => {
-        console.log(err.response.data);
-    });
-
-axios.post('http://localhost:3000/books', {
-    ID: 2,
-    name: 'Os Lusiadas',
-    author: 'Luis de Camoes'
-})
-    .then((result) => {
-        console.log(result.data);
-    })
-    .catch(error => {
-        console.log(error.response.data);
-    });
-
-axios.get('http://localhost:3000/books')
-    .then((response) => {
-        response.data.map((aux) => {
-            axios.get('http://localhost:3000/books/' + aux.ID)
-                .then((response) => {
-                    console.log(response.data);
-                });
+        .then((res) => {
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.log(err.response.data);
         });
-    });
-    
-axios.get('http://localhost:3000/log')
-    .then((response) => {
-        console.log(response.data);
-    });
+}
+
+async function addExistentBook() {
+    await axios.post('http://localhost:3000/books', {
+        ID: 2,
+        name: 'Os Lusiadas',
+        author: 'Luis de Camoes'
+    })
+        .then((res) => {
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.log(err.response.data);
+        });
+}
+
+async function readBooks() {
+    await axios.get('http://localhost:3000/books')
+        .then((response) => {
+            response.data.map((el) => {
+                axios.get('http://localhost:3000/books/' + el.ID)
+                    .then((response) => {
+                        console.log(response.data);
+                    });
+            });
+        });
+}
+
+async function readLog() {
+    await axios.get('http://localhost:3000/log')
+        .then((response) => {
+            console.log(response.data);
+        });
+}
+
+addNewBook();
+addExistentBook();
+readBooks();
+readLog();
